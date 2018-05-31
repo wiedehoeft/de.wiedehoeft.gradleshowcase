44278fbea1fde46412b2d2984bf13797c82ab802
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-05-31 15:19:23 +0200

    Second Excercise: Step Four: Use new format amount method for printing rental overview

--- de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/Customer.java
+++ de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/Customer.java
@@ -24,7 +24,7 @@ public class Customer {
         return "Kill Bill: EUR 3.00\n" +
                 "Star Wars: EUR 4.50\n" +
                 "The Minions: EUR 3.00\n" +
-                "Total Charge: EUR " + totalAmount + "0";
+                "Total Charge: EUR " + prettyPrint(totalAmount);
     }
 
     public String prettyPrint(double amount) {
