43e72b8071abaaeff575d2d7f771fb3794d1c950
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-05-31 14:52:20 +0200

    Refactoring: Dont use magic numbers

--- de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/Customer.java
+++ de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/Customer.java
@@ -2,13 +2,16 @@ package de.wiedehoeft.librarykata;
 
 public class Customer {
 
+    private static final double BASE_PRICE = 3.00;
+    private static final int DAYS_DISCOUNTED = 3;
+    private static final double ADDITIIONAL_PRICE = 1.50;
     private double totalAmount;
 
     public double rentMovie(int days) {
-        totalAmount += 3.00;
+        totalAmount += BASE_PRICE;
 
-        if (days > 3) {
-            totalAmount += 1.50 * (days - 3);
+        if (days > DAYS_DISCOUNTED) {
+            totalAmount += ADDITIIONAL_PRICE * (days - DAYS_DISCOUNTED);
         }
 
         return totalAmount;
