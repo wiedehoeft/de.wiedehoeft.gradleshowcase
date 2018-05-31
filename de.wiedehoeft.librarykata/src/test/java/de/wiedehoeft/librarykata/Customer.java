ae8ff7ca60a978c7c76fa974ef92d9ac615cbb97
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-05-31 14:47:06 +0200

    Testcase for renting a movie

--- de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/Customer.java
+++ de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/Customer.java
@@ -1,4 +1,10 @@
 package de.wiedehoeft.librarykata;
 
-public class Rental {
+public class Customer {
+
+    private double totalAmount;
+
+    public double rentMovie(int days) {
+        return totalAmount += 3;
+    }
 }
--- de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/RentalTest.java
+++ de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/RentalTest.java
@@ -1,4 +1,20 @@
 package de.wiedehoeft.librarykata;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RentalTest {
+
+    @Test
+    public void rentMovieForOneDay() {
+        // Given
+        Customer customer = new Customer();
+
+        // When
+        final double amount = customer.rentMovie(1);
+
+        // Then
+        assertThat(amount).isEqualTo(3.00);
+    }
 }
