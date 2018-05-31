692059a121f4202920192b36e1f67b65cf198a32
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-05-31 14:54:35 +0200

    Refactoring: Use setUp method and expect test style

--- de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/RentalTest.java
+++ de.wiedehoeft.librarykata/src/test/java/de/wiedehoeft/librarykata/RentalTest.java
@@ -1,58 +1,46 @@
 package de.wiedehoeft.librarykata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RentalTest {
 
+    private Customer customer;
+
+    @Before
+    public void setUp() throws Exception {
+        customer = new Customer();
+    }
+
     @Test
     public void rentMovieForOneDay() {
-        // Given
-        Customer customer = new Customer();
 
-        // When
-        final double amount = customer.rentMovie(1);
-
-        // Then
-        assertThat(amount).isEqualTo(3.00);
+        // Expect
+        assertThat(customer.rentMovie(1)).isEqualTo(3.00);
     }
 
     @Test
     public void rentMovieForThreeDays() {
-        // Given
-        Customer customer = new Customer();
 
-        // When
-        final double amount = customer.rentMovie(2);
-
-        // Then
-        assertThat(amount).isEqualTo(3.00);
+        // Expect
+        assertThat(customer.rentMovie(3)).isEqualTo(3.00);
     }
 
 
     @Test
     public void rentMovieForFourDays() {
-        // Given
-        Customer customer = new Customer();
 
-        // When
-        final double amount = customer.rentMovie(4);
-
-        // Then
-        assertThat(amount).isEqualTo(4.50);
+        // Expect
+        assertThat(customer.rentMovie(4)).isEqualTo(4.50);
     }
 
 
     @Test
     public void rentMovieForFiveDays() {
-        // Given
-        Customer customer = new Customer();
 
-        // When
-        final double amount = customer.rentMovie(5);
-
-        // Then
-        assertThat(amount).isEqualTo(6.00);
+        // Expect
+        assertThat(customer.rentMovie(5)).isEqualTo(6.00);
     }
 }
